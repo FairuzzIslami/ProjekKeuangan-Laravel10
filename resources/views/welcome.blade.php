f44e1586ba62e7ba716136625921e07df7f97bfc
Fairuz Aqila Islami <firdauz2007123@gmail.com>
2025-09-10 16:07:52 +0700

    add layout in view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Dompetku - Kelola Keuanganmu dengan Mudah</title>
-  <link rel="icon" type="image/x-icon" href="./img/logo.png">
+  <link rel="icon" type="image/x-icon" href="{{asset('asset/img/logo.png')}}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
